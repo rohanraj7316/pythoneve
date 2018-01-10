0846a784637b39e10a4c63471cc0c0ad4461e877
unio-7316 <rohanraj7316@gmail.com>
2018-01-10 20:22:11 +0530

    Simple server has been created using python

---
+++ run.py
@@ -0,0 +1,5 @@
+from eve import Eve
+
+app = Eve(settings="my_settings.py")
+if __name__ == '__main__':
+    app.run()
---
+++ settings.py
@@ -0,0 +1,11 @@
+
+# connect to mongo
+
+MONGO_HOST
+
+
+# just a couple of api endpoints with no custom options and validation rules.
+DOMAIN = {
+    'people': {},
+    'books': {}
+}
